afcc2e93d5ca4de8b71ffa23a8249431c398fdb9
Muhammad Ammar Cheema <muhammadammar7747@gmail.com>
2024-06-10 03:25:44 +0500

    FrontEnd Started

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Perspective Pulse</h1>
+      <p>React is a JavaScript library for building user interfaces.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <>
+            <h1>Perspective Pulse</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </>
+    );
+}
+export default Navbar;
