7c503cbc45b0a6cebcaf7ef87bd70d51087e8a74
Muhammad Ammar Cheema <muhammadammar7747@gmail.com>
2024-07-08 16:00:10 +0500

    Login Page

--- frontend/src/Components/TextInput/textInput.jsx
+++ frontend/src/Components/TextInput/textInput.jsx
@@ -4,7 +4,7 @@ function textInput(props){
     return(
         <div className={styles.textInputWrapper}>
             <input {...props} />
-            {props.error && <p className={styles.errorMessage}>{props.errorMessage}</p>}
+            {props.error && (<p className={styles.errorMessage}>{props.errormessage}</p>)}
         </div>
     );
 }
--- frontend/src/Pages/Login/login.jsx
+++ frontend/src/Pages/Login/login.jsx
@@ -1,14 +1,49 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import styles from './login.module.css';
-// import textInput from '../../Components/TextInput/textInput';
+import TextInput from '../../Components/TextInput/textInput';
+import { useFormik } from 'formik';
+import loginSchema from '../../Schemas/loginSchema';
 
 function login() {
-    <div className={styles.loginWrapper}>
-        <div className={styles.loginHeader}>Log in to your Account</div>
-        <textInput />
-        <textInput />
-        <button className={styles.loginButton}>Log in</button>
-        <span className={styles.loginFooter}>Don't have an account? <button href='/signup'>Register</button></span>
-    </div>
+
+    const {values, touched, handleBlur, handleChange, errors} = useFormik({
+        initialValues: {
+            username: '',
+            password: ''
+        },
+        validationSchema: loginSchema,
+        onSubmit: (values) => {
+            console.log(values);
+        }
+    });
+
+    return (
+        <div className={styles.loginWrapper}>
+            <div className={styles.loginHeader}>Log In to your Account</div>
+            <TextInput 
+                type='text'
+                name='username'
+                value = {values.username}
+                onBlur = {handleBlur}
+                onChange = {handleChange}
+                placeholder = 'Username'
+                error = {errors.username && touched.username ? 1: undefined}
+                errormessage = {errors.username}
+            />
+            <TextInput
+                type='password'
+                name='password'
+                value = {values.password}
+                onBlur = {handleBlur}
+                onChange = {handleChange}
+                placeholder = 'Password'
+                error = {errors.password && touched.password ? 1: undefined}
+                errormessage = {errors.password}
+            />
+            <button className={styles.logInButton}>Log In</button>
+            <span>Don't have an account?{" "} <button className={styles.createAccount} href='/signup'>Register</button></span>
+        </div>
+    )
 }
 
 export default login;
